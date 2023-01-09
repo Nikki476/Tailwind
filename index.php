7a0f02dc8cc4ea916f5c546e800b5a6481acc5a7
Nikki476 <nikhitha1999in@gmail.com>
2023-01-09 16:22:51 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="public/css/tailwind.css" rel="stylesheet">
+</head>
+<body>
+    <h1 class="text-red-700">Hello world..</h1>
+    <?php
+        echo "welcome";
+
+    ?>
+    <div class="border-b border-gray-200 pb-5 bg-red-800">
+  <h3 class="text-lg font-medium leading-6 text-gray-900">Job Postings</h3>
+  <p class="mt-2 max-w-4xl text-sm text-gray-500">Workcation is a property rental website. Etiam ullamcorper massa viverra consequat, consectetur id nulla tempus. Fringilla egestas justo massa purus sagittis malesuada.</p>
+</div>
+</body>
+</html>
